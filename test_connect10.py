8ac172d2fcd756be3559849c3e39159b959c9a47
thespuds <erik.shear@gmail.com>
2016-07-06 13:48:37 -0600

    Add files via upload

--- test_connect10.py
+++ test_connect10.py
@@ -20,20 +20,23 @@ def how_connect(ip):
             port = 23
         except socket.error:
             pass
-            
+
     finally:
         if port == 22:
-            print(ip, ' connects via SSH')
+            #Generate a Log file of devices that can connect via
+            print(ip, 'connects via SSH')
             ssh_log = open('ssh_log.txt', 'a')
             ssh_log.write(ip + "\n")
             ssh_log.close()
         elif port == 23:
-            print(ip, ' connects via Telnet')
+            #Generate a Log file of devices that can connect via Telnet.
+            print(ip, 'connects via Telnet')
             telnet_log = open('telnet_log.txt', 'a')
             telnet_log.write(ip + "\n")
             telnet_log.close()
         else:
-            print(ip, ' cannot connect via either SSH or TELNET!')
+            #generates a log file containing unreachable devices.
+            print(ip, 'cannot connect via either SSH or TELNET!')
             error_log = open('error_log.txt', 'a')
             error_log.write(ip + "\n")
             error_log.close()
@@ -51,5 +54,11 @@ selected_ip_file = open(ip_file, 'r')
 selected_ip_file.seek(0)
 #read each line in the file and assign it to ip_list
 ip_list = selected_ip_file.read().splitlines()
+
+#Check each ip in a list.
 for ip in ip_list:
     how_connect(ip)
+
+#Clean up and exit
+selected_ip_file.close()
+sys.exit()
